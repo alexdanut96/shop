fe8cb1861bbf05c630e1955bfd9ccb9da683e996
alexdanut96 <alexandruburcea1996@gmail.com>
2023-09-25 23:29:36 +0300

    secure branch in client-side with dotenv variables

--- client-side/src/ApiRequests.js
+++ client-side/src/ApiRequests.js
@@ -1,7 +1,6 @@
 const allProductsApi = "http://localhost:5000/api/products";
 const productApi = "http://localhost:5000/api/products/find/";
 const categoryProductsApi = "http://localhost:5000/api/products?category=";
-const token =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OTUzZjUxNzA4ZDk5NjA1OTVlZWQ0ZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5NTUxMTM2MiwiZXhwIjoxNjk1NzcwNTYyfQ.K-GdQdDP7ML6kli6Z14IiVsXJVg0c8wUEyrX4lAwNRk";
+const token = process.env.REACT_APP_TOKEN;
 
 export { allProductsApi, categoryProductsApi, productApi, token };
