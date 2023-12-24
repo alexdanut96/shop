d5e45641ef3b32a0473f398c2359712b55dbd932
alexdanut96 <alexandruburcea1996@gmail.com>
2023-12-24 23:20:48 +0200

    productsList progress

--- admin/src/pages/NewProduct.jsx
+++ admin/src/pages/NewProduct.jsx
@@ -1,29 +1,119 @@
+import { useState, useEffect } from "react";
+
 const NewProduct = () => {
+  const [titleValidationError, setTitleValidationError] = useState(false);
+  const [descriptionValidationError, setDescriptionValidationError] =
+    useState(false);
+
   return (
     <div className="right-side-container">
       <div className="newProduct-container animated">
-        <h1 className="addProductTitle">New Product</h1>
-        <form className="addProductForm">
-          <div className="addProductItem">
-            <label>Image</label>
-            <input type="file" id="file" />
+        <div className="newProductTitle">New Product</div>
+        {/* Form */}
+        <form>
+          <div className="form-group field">
+            {/* title */}
+            <div className="wrapper">
+              <label htmlFor="title" className="form-label">
+                Title *
+              </label>
+              <div className="input-wrapper">
+                <input
+                  type="text"
+                  className={`form-field title ${
+                    titleValidationError ? "error" : ""
+                  }`}
+                  placeholder="Title"
+                  name="title"
+                  id="title"
+                  required
+                />
+              </div>
+              {titleValidationError ? (
+                <div className="username error-message">
+                  This field is required!
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
+            {/* description */}
+            <div className="wrapper">
+              <label htmlFor="description" className="form-label">
+                Description *
+              </label>
+              <div className="input-wrapper">
+                <input
+                  type="text"
+                  className={`form-field title ${
+                    descriptionValidationError ? "error" : ""
+                  }`}
+                  placeholder="Description"
+                  name="description"
+                  id="description"
+                  required
+                />
+              </div>
+              {descriptionValidationError ? (
+                <div className="description error-message">
+                  This field is required!
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
+            {/* size */}
+            <div className="wrapper">
+              <label htmlFor="description" className="form-label">
+                Size *
+              </label>
+              <div className="input-wrapper">
+                <select>
+                  <option>XS</option>
+                  <option>S</option>
+                  <option>M</option>
+                  <option>L</option>
+                  <option>XL</option>
+                </select>
+              </div>
+              {descriptionValidationError ? (
+                <div className="description error-message">
+                  This field is required!
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
+
+            {/* category */}
+            <div className="wrapper">
+              <label htmlFor="category" className="form-label">
+                Category *
+              </label>
+              <div className="input-wrapper">
+                <select>
+                  <option>XS</option>
+                  <option>S</option>
+                  <option>M</option>
+                  <option>L</option>
+                  <option>XL</option>
+                </select>
+              </div>
+              {descriptionValidationError ? (
+                <div className="category error-message">
+                  This field is required!
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
           </div>
-          <div className="addProductItem">
-            <label>Name</label>
-            <input type="text" placeholder="Apple Airpods" />
-          </div>
-          <div className="addProductItem">
-            <label>Stock</label>
-            <input type="text" placeholder="123" />
-          </div>
-          <div className="addProductItem">
-            <label>Active</label>
-            <select name="active" id="active">
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-            </select>
-          </div>
-          <button className="addProductButton">Create</button>
+          <button
+            //  onClick={addNewUser}
+            className="save-changes"
+          >
+            Save changes
+          </button>
         </form>
       </div>
     </div>
